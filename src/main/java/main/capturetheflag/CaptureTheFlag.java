e80607c6a0b98af9efbd60a3df4e1d8d7888da5b
wolkensteine <105732359+Wolkensteine@users.noreply.github.com>
2023-08-09 10:22:53 +0200

    Initial commit

---
+++ src/main/java/main/capturetheflag/CaptureTheFlag.java
@@ -0,0 +1,17 @@
+package main.capturetheflag;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CaptureTheFlag extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
---
+++ src/main/java/main/capturetheflag/Map.java
@@ -0,0 +1,19 @@
+package main.capturetheflag;
+
+public class Map {
+
+    // The following 2 arrays define the volume in which the map is
+    // begins[0] would be the beginning x-position
+    // begins[1] would be the beginning y-position
+    // begins[2] would be the beginning z-position
+    // ends[0] would be the ending x-position
+    // ends[1] would be the ending y-position
+    // ends[2] would be the ending z-position
+    private int[] begins;
+    private int[] ends;
+
+    public Map () {
+
+    }
+
+}
