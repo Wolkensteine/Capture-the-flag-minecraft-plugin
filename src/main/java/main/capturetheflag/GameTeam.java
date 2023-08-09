e5595e115a66af0ba426ad2b1aa85bf93a2d713d
wolkensteine <105732359+Wolkensteine@users.noreply.github.com>
2023-08-09 11:52:20 +0200

    Adding getters and setters to GameTeam

--- src/main/java/main/capturetheflag/GameTeam.java
+++ src/main/java/main/capturetheflag/GameTeam.java
@@ -6,13 +6,31 @@ import org.bukkit.entity.Player;
 public class GameTeam {
 
     // The following variable stores the members of a team
-    private Player members;
+    private final Player[] members;
 
     // Defines the team colour
-    final Color color;
+    private final Color color;
 
-    public GameTeam (Color team_color) {
+    public GameTeam (Color team_color, Player[] team_members) {
+        // Assigning values to the variables
+        // The team members will be either randomly assigned on game start or via a game command
         color = team_color;
+        members = team_members;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public boolean isPlayerMember(Player player) {
+        boolean tmp = false;
+        for (Player member : members) {
+            if (member == player) {
+                tmp = true;
+                break;
+            }
+        }
+        return tmp;
     }
 
 }
