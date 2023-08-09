280cd1df3fffbdf3730baa2bcef97efee6b66ce8
wolkensteine <105732359+Wolkensteine@users.noreply.github.com>
2023-08-09 11:29:49 +0200

    Finished FlagSpot class

--- src/main/java/main/capturetheflag/FlagSpot.java
+++ src/main/java/main/capturetheflag/FlagSpot.java
@@ -3,19 +3,34 @@ package main.capturetheflag;
 public class FlagSpot {
 
     // Stores position
-    private int[] coords;
+    private final int[] coords;
 
     // Stores type
     // Can be placeable_op or placeable_ho
     // placeable_op is for the opponent team (so capturing a flag)
     // placeable_ho is for the home team (so the base position of a flag)
-    private String type;
+    private final String type;
 
     // Stores the team to which the spot belongs
-    private GameTeam team;
+    private final GameTeam team;
 
-    public FlagSpot () {
+    public FlagSpot (int[] given_coords, GameTeam assigned_team, String assigned_type) {
+        // Defining variables which are given via a command
+        coords = given_coords;
+        type = assigned_type;
+        team = assigned_team;
+    }
 
+    public int[] getCoords() {
+        return coords;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public GameTeam getTeam() {
+        return team;
     }
 
 }
