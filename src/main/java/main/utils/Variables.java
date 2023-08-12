9c11c6e80ef01ae6e80393509459137324764efe
wolkensteine <105732359+Wolkensteine@users.noreply.github.com>
2023-08-12 12:12:24 +0200

    Variables
    Bug fix

--- src/main/java/main/utils/Variables.java
+++ src/main/java/main/utils/Variables.java
@@ -8,7 +8,7 @@ public class Variables {
     // In this class global variables will be stored that will be used in multiple different classes and methods
 
     public static Map currentMap; // Map object of the currently active map
-    public static Map[] maps; // All the maps there are
+    public static Map[] maps = new Map[0]; // All the maps there are
 
     // The following variables are set via a command and will be used to create a map.
     public static int[] pos1 = new int[3]; // Selectable position 1
