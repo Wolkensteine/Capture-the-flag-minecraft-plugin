9a0bc13c40313b94d82982b22281d6d6a5bed10a
wolkensteine <105732359+Wolkensteine@users.noreply.github.com>
2023-08-12 11:54:43 +0200

    Command update
    Actually adding the command on enable

--- src/main/java/main/capturetheflag/CaptureTheFlag.java
+++ src/main/java/main/capturetheflag/CaptureTheFlag.java
@@ -3,11 +3,14 @@ package main.capturetheflag;
 import main.capturetheflag.listeners.BlockEventListener;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import java.util.Objects;
+
 public final class CaptureTheFlag extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        getServer().getPluginManager().registerEvents(new BlockEventListener(), this);
+        getServer().getPluginManager().registerEvents(new BlockEventListener(), this); // Add BlockEventListener
+        Objects.requireNonNull(this.getCommand("CTF")).setExecutor(new Commands()); // Add the main command
     }
 
     @Override
